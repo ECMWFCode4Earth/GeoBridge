2bf53a7f330594bca7765702dfb5f46f86522473
KonstantinosF <fkwstas@gmail.com>
2026-08-21 17:27:16 +0300

    Removed files

--- examples/test_discovery.py
+++
@@ -1,9 +0,0 @@
-import geobridge as gb
-# All ARCO datasets (fast extraction)
-print("ARCO only:")
-for ds in gb.discover(arco_only=True):
-    print(ds.id)
-# All datasets that can be extracted (ARCO + CDS API)
-print("\n Extracation only:")
-for ds in gb.discover(extraction_only=True):
-    print(ds.id, "ARCO" if ds.has_zarr else "CDS API")
